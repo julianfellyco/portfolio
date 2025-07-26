36b3b8f7edab11e9832f314fd369564142ccac0c
Julian Fellyco <74457155+julianfellyco@users.noreply.github.com>
2025-07-27 05:22:20 +1000

    Delete Modal.js

--- Modal.js
+++
@@ -1,7 +0,0 @@
-export default function Modal() {
-  return (
-    <section className="min-h-screen flex items-center justify-center">
-      <h2 className="text-3xl font-bold">Modal Section</h2>
-    </section>
-  )
-}
