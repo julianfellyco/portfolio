8e9b39f2f75b6200b599dd37aed0e7dc29c43bda
Julian Fellyco <74457155+julianfellyco@users.noreply.github.com>
2025-07-27 05:26:29 +1000

    Delete components directory

--- components/ProjectsSection.js
+++
@@ -1,7 +0,0 @@
-export default function ProjectsSection() {
-  return (
-    <section className="min-h-screen flex items-center justify-center">
-      <h2 className="text-3xl font-bold">ProjectsSection Section</h2>
-    </section>
-  )
-}
