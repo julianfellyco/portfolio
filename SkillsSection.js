2f6d93d46741e59293cf288e5ee62a1b88fb91c7
Julian Fellyco <74457155+julianfellyco@users.noreply.github.com>
2025-07-27 05:22:47 +1000

    Delete SkillsSection.js

--- SkillsSection.js
+++
@@ -1,7 +0,0 @@
-export default function SkillsSection() {
-  return (
-    <section className="min-h-screen flex items-center justify-center">
-      <h2 className="text-3xl font-bold">SkillsSection Section</h2>
-    </section>
-  )
-}
